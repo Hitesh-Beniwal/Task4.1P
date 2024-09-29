4b67a6073fb04c2336eb17d01eae21b5f32b46a3
Hitesh Beniwal <hitesh0001beniwal@gmail.com>
2024-09-29 19:43:39 +0530

    Update SignUp.jsx

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,11 +1,26 @@
-function Form(){
-    return(
-        <form action="/" method="post" className="form-signup">
-            <label id="email">SIGN UP FOR OUR DAILY INSIDER</label>
-            <input type="email" class="email" name="email" placeholder="Enter your email" required></input>
-            <button type="submit">Subscribe</button>
-        </form>
-    )
+import React from 'react';
+
+function Form() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    console.log("Form submitted!");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <label htmlFor="email">SIGN UP FOR OUR DAILY INSIDER</label>
+      <input 
+        type="email" 
+        className="email" 
+        name="email" 
+        id="email" 
+        placeholder="Enter your email" 
+        required 
+      />
+      <button type="submit">Subscribe</button>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
