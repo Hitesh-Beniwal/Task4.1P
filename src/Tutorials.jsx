3c435d65951db9ddb1721a0ea24cb94bf08c7bf8
Hitesh Beniwal <hitesh0001beniwal@gmail.com>
2024-09-29 19:44:16 +0530

    Update Tutorials.jsx

--- src/Tutorials.jsx
+++ src/Tutorials.jsx
@@ -1,20 +1,43 @@
-import {Article, ArticleObj} from "./Article";
-
-const John = "Emerging technologies in software development are revolutionizing how applications are built and utilized. Artificial Intelligence (AI) and Machine Learning (ML) are enhancing software with capabilities for data-driven insights, automation, and personalization."
-
-var tutorial1 = new ArticleObj("./images/ml.jpg", "Tutorial 1", "Machine Learning", John, 4.7, "Max");
-var tutorial2 = new ArticleObj("./images/kub2.jpeg", "Tutorial 2", "Kubernetes", John,4.9, "Yuan");
-var tutorial3 = new ArticleObj("./images/type.png", "Tutorial 3", "Typscript", John,3.2, "Kevin");
+import React from 'react';
+import { Article } from './Article';  
 
 
-function Tutorials(){
-    return(
-        <div className="all-articles">
-        <Article article={tutorial1} />
-        <Article article={tutorial2} />
-        <Article article={tutorial3} />
-        </div>
-    )
+const tutorials = [
+  {
+    src: "./images/ml.jpg",
+    alt: "Tutorial 1",
+    title: "Machine Learning",
+    description: "Emerging technologies in software development are revolutionizing how applications are built and utilized. Artificial Intelligence (AI) and Machine Learning (ML) are enhancing software with capabilities for data-driven insights, automation, and personalization.",
+    rating: 4.7,
+    author_name: "Max"
+  },
+  {
+    src: "./images/kub2.jpeg",
+    alt: "Tutorial 2",
+    title: "Kubernetes",
+    description: "Emerging technologies in software development are revolutionizing how applications are built and utilized. Artificial Intelligence (AI) and Machine Learning (ML) are enhancing software with capabilities for data-driven insights, automation, and personalization.",
+    rating: 4.9,
+    author_name: "Yuan"
+  },
+  {
+    src: "./images/type.png",
+    alt: "Tutorial 3",
+    title: "Typescript",
+    description: "Emerging technologies in software development are revolutionizing how applications are built and utilized. Artificial Intelligence (AI) and Machine Learning (ML) are enhancing software with capabilities for data-driven insights, automation, and personalization.",
+    rating: 3.2,
+    author_name: "Kevin"
+  }
+];
+
+
+function Tutorials() {
+  return (
+    <div className="all">
+      {tutorials.map((tutorial, index) => (
+        <Article key={index} article={tutorial} />
+      ))}
+    </div>
+  );
 }
 
-export default Tutorials
+export default Tutorials;
